9c2ed4587c72220e5860168c03d8d141a4344749
Nina-a <nina.leduc@gmail.com>
2021-01-07 16:07:02 +0100

    works to 5 cards

--- poker.js
+++ poker.js
@@ -1,3 +1,6 @@
+import { xor } from "lodash";
+import { xit } from "mocha";
+
 let black = ['2H', '3D', '5S', '9C', 'KD'];
 
 let white = ['2C', '3H', '4S', '8C', 'AH'];
@@ -6,14 +9,28 @@ let order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'K', 'A'];
 
 function pokerResult(hand1, hand2) {
   // Remplir un tableau avec la première valeur de la carte
-  let valeurcardhand1 =  hand1[0].substr(0,1);
-  let valeurcardhand2 =  hand2[0].substr(0,1);
+  let valeurcardhand1 = [];
+
+  for ( var i = 0 ; i < hand1.length ; i++ ){
+    let valeur = hand1[i].substr(0,1);
+    valeurcardhand1.push(valeur);
+  }
+   
+  let valeurcardhand2 = [];
+
+  for ( var i = 0 ; i < hand2.length ; i++ ){
+    let valeur = hand2[i].substr(0,1);
+    valeurcardhand2.push(valeur);
+  }
 
   // Remplir un second tableau (fonction map) pour déterminer l'ordre des cartes
-  let ordrecard1 = order.indexOf(valeurcardhand1);
-  let ordrecard2 = order.indexOf(valeurcardhand2);
+  let ordrecard1 = parseInt(valeurcardhand1.map( x => order.indexOf(x)));
+  let ordrecard2 = parseInt(valeurcardhand2.map( x => order.indexOf(x)));
 
-  let result = ordrecard1 > ordrecard2 ?  '1':  '2';
+  let maxcard1 = Math.max(ordrecard1);
+  let maxcard2 = Math.max(ordrecard2);
+
+  let result = maxcard1 > maxcard2 ?  '1':  '2';
   return result;
 }
 export { pokerResult };
--- test/rules.tests.js
+++ test/rules.tests.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 
 describe('Poker Hand', function () {
   it('la plus haute carte gagne', () => {
-    expect(pokerResult(['2H', '3D', '5S', '9C', 'KD'], ['2C', '3H', '4S', '8C', 'AH'])).to.be.equal('1');
+    expect(pokerResult(['2H', '3D', '5S', '9C', 'KD'], ['2C', '3H', '4S', '8C', 'AH'])).to.be.equal('2');
   });
 
   // it('should evaluate a hand as high card', function () {
