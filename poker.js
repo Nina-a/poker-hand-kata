8bde8eb1a133e1c4b0691713ac63df88d0c2e6e4
Nina-a <nina.leduc@gmail.com>
2021-01-11 15:05:47 +0100

    works for all pair

--- poker.js
+++ poker.js
@@ -1,70 +1,117 @@
-import { forIn } from "lodash";
-
-let black = ['2H', '3D', '5S', '9C', 'KD'];
-
-let white = ['2C', '3H', '4S', '8C', 'AH'];
-
-let order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'K', 'A'];
+let order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T','J','Q','K','A'];
 
 function pokerResult(hand1, hand2) {
   // Remplir un tableau avec la valeur de la carte
   let valeurCardHand1 = [];
-  for ( var i = 0 ; i < hand1.length ; i++ ){
-    let valeur = hand1[i].substr(0,1);
+  for (var i = 0; i < hand1.length; i++) 
+  {
+    let valeur = hand1[i].substr(0, 1);
     valeurCardHand1.push(valeur);
   }
-  
+
   let valeurCardHand2 = [];
-  for ( var i = 0 ; i < hand2.length ; i++ ){
-    let valeur = hand2[i].substr(0,1);
+  for (var i = 0; i < hand2.length; i++) 
+  {
+    let valeur = hand2[i].substr(0, 1);
     valeurCardHand2.push(valeur);
   }
+
+  // tableau convertissant la valeur de la carte en ordre de la plus petite à la plus grande
+  let ordreCard1 =(valeurCardHand1.map((x) => order.indexOf(x)));
+  let ordreCard2 = (valeurCardHand2.map((x) => order.indexOf(x)));
+
   // Tableau permettant de connaitre le nombre de fois ou l'on la même carte
   let numberOfSameCard1 = [];
-  for ( var i = 0 ; i < valeurCardHand1.length ; i++ ){
-    if (numberOfSameCard1[valeurCardHand1[i]] )
-    { numberOfSameCard1[valeurCardHand1[i]] = numberOfSameCard1[valeurCardHand1[i]] + 1 ; } 
-    else { numberOfSameCard1[valeurCardHand1[i]] = 1 ; } 
-  }
-  let numberOfSameCard2 = [];
-  for ( var i = 0 ; i < valeurCardHand2.length ; i++ ){
-    if (numberOfSameCard2[valeurCardHand2[i]] )
-    { numberOfSameCard2[valeurCardHand2[i]] = numberOfSameCard2[valeurCardHand2[i]] + 1 ; } 
-    else { numberOfSameCard2[valeurCardHand2[i]] = 1 ; } 
-  }
-
-  // Il n'y a que des cartes simples dans les mains
-  if ( numberOfSameCard1.indexOf(2) == -1 & numberOfSameCard1.indexOf(2) == -1 )
+  for (var i = 0; i < ordreCard1.length; i++) 
   {
-    // faire un tableau (fonction map) pour déterminer l'ordre des cartes
-    let ordreCard1 = parseInt(valeurCardHand1.map( x => order.indexOf(x)));
-    let ordreCard2 = parseInt(valeurCardHand2.map( x => order.indexOf(x)));
-    // Trouve la carte la plus grande
-    let maxcard1 = Math.max(ordreCard1);
-    let maxcard2 = Math.max(ordreCard2);
+    if (numberOfSameCard1[ordreCard1[i]]) 
+    {
+      numberOfSameCard1[ordreCard1[i]] =
+        numberOfSameCard1[ordreCard1[i]] + 1;
+    }
+    else 
+    {
+      numberOfSameCard1[ordreCard1[i]] = 1;
+    }
+  }
 
-    let result = maxcard1 > maxcard2 ?  '1':  '2';
+  let numberOfSameCard2 = [];
+  for (var i = 0; i < ordreCard2.length; i++) 
+  {
+    if (numberOfSameCard2[ordreCard2[i]]) 
+    {
+      numberOfSameCard2[ordreCard2[i]] =
+        numberOfSameCard2[ordreCard2[i]] + 1;
+    } 
+    else 
+    {
+      numberOfSameCard2[ordreCard2[i]] = 1;
+    }
+  }
+
+  // Il y a au moins une paire dans chaque main
+  if ((numberOfSameCard1.indexOf(2) !== -1) && (numberOfSameCard2.indexOf(2) !== -1)) 
+  {
+    var paire1 = [];
+    var paire2 = [];
+
+    // trouver la paire par main
+    for (var valeur in numberOfSameCard1) 
+    {
+      if (numberOfSameCard1[valeur] == 2 ) 
+      {
+        paire1.push(valeur);
+      }
+    }
+    let maxPaire1 = Math.max(...paire1);
+    let nombreOfPaire1 = paire1.length;
+  
+    for (var valeur in numberOfSameCard2) 
+    {
+      if (numberOfSameCard2[valeur] == 2) 
+      {
+        paire2.push(valeur);
+      }
+    }
+    let maxPaire2 = Math.max(...paire2);
+    let nombreOfPaire2 = paire2.length;
+
+    // Si les paires max ne sont pas les mêmes
+    if (( nombreOfPaire1 == nombreOfPaire2)  )
+    {
+      if(maxPaire1 == maxPaire2 )
+      {
+        var result = paire1[0]>paire2[0] ? '1' : '2';
+      }
+      else
+      {
+        var result = maxPaire1 > maxPaire2 ? '1' : '2';
+      }
+    }
+    else
+    {
+      var result = maxPaire1 > maxPaire2 ? '1' : '2';
+    }
     return result;
   }
-  // Il y a au moins une paire dans chaque main
-  else if ( numberOfSameCard1.indexOf(2) !== -1 & numberOfSameCard1.indexOf(2) !== -1)
+ //Il y a au moins une paire dans une main et des cartes simples dans l'autre
+ else if(!((numberOfSameCard1.indexOf(2) == -1) && (numberOfSameCard2.indexOf(2) == -1)))
   {
-    // trouver la paire par main
-  for ( var valeur in numberOfSameCard1){
-    if (numberOfSameCard1[valeur] == 2){
-      var Paire1 = 0;
-      valeur > Paire1 ? Paire1 = valeur : Paire1 = Paire1;
-    }
-  }
-  for ( var valeur in numberOfSameCard2){
-    if (numberOfSameCard2[valeur] == 2){
-      var Paire2 = 0;
-      valeur > Paire2 ? Paire2 = valeur : Paire = Paire2;
-    }
-  }
-  let result = Paire1 > Paire2 ?  '1':  '2';
-  return result;
-} 
 
+    let result = numberOfSameCard1.indexOf(2) !== -1 ? '1': '2';
+    return result;
+  }
+  // Il n'y a que des cartes simples dans les mains
+  else if ((numberOfSameCard1.indexOf(2) == -1) && (numberOfSameCard1.indexOf(2) == -1)) 
+  {
+    // faire un tableau (fonction map) pour déterminer l'ordre des cartes
+
+    // Trouve la carte la plus grande
+    let maxcard1 = Math.max(...ordreCard1);
+    let maxcard2 = Math.max(...ordreCard2);
+
+    let result = maxcard1 > maxcard2 ? '1' : '2';
+    return result;
+  }
 }
 export { pokerResult };
--- test/rules.tests.js
+++ test/rules.tests.js
@@ -3,20 +3,98 @@ import { pokerResult } from '../poker.js';
 const expect = require('chai').expect;
 
 describe('Poker Hand', function () {
-  it('la plus haute carte gagne', () => {
-    expect(pokerResult(['2H', '3D', '5S', '9C', 'KD'], ['2C', '3H', '4S', '8C', 'AH'])).to.be.equal('2');
+
+  describe('Single Cars', function(){
+    it('la plus haute carte gagne #1', () => {
+      expect(
+        pokerResult(
+          ['2H', '3D', '5S', '9C', 'KD'], // 1 gagne avec K
+          ['2C', '3H', '4S', '8C', 'QH']
+        )
+      ).to.be.equal('1');
+    });
+
+    it('la plus haute carte gagne #2', () => {
+      expect(
+        pokerResult(
+          ['2H', '3D', '5S', '9C', 'KD'],
+          ['2C', '3H', '4S', '8C', 'AH'] // 2 gagne avec A
+        )
+      ).to.be.equal('2');
+    });
+  })
+
+  describe('Pair', function () {
+    it('la plus grande paire gagne #1', () => {
+      expect(
+        pokerResult(
+          ['2H', '4S', '9S', 'KC', 'KD'], // 1 gagne avec la paire de roi
+          ['2C', '3H', '5S', '5C', 'AH']
+        )
+      ).to.be.equal('1');
+    });
+
+    it('la plus grande paire gagne #2', () => {
+      expect(
+        pokerResult(
+          ['2C', '3H', '4S', '4C', 'AH'],
+          ['2H', '2S', '9S', 'KC', 'KD'] // 2 gagne avec la paire de Roi
+        )
+      ).to.be.equal('2');
+    });
+
+     it('la main contenant au moins une paire contre des simples cartes gagne #1', () => {
+      expect(
+        pokerResult(
+          ['2H', '3S', '9S', '9C', 'KD'], // gagne avec une paire de 9
+          ['2C', '3H', '4S', '5C', 'TH']
+        )
+      ).to.be.equal('1');
+    });
+
+    it('la main contenant au moins une paire contre des simples cartes gagne #2', () => {
+      expect(
+        pokerResult(
+          ['2H', '3S', '5S', '9C', 'KD'],
+          ['2C', '3H', '4S', 'TC', 'TH']// gagne avec une paire de 10
+        )
+      ).to.be.equal('2');
+    });
+
+    it('la main contenant 2 paires contre 1 paire gagne #1 ', () => {
+      expect(
+        pokerResult(
+          ['2H', '2S', '9S', '9C', 'KD'],// double paire de 9 et de 2
+          ['2C', '3H', '3S', '5C', 'TH']
+        )
+      ).to.be.equal('1');
+    });
+
+    it('la main contenant 2 paires contre 1 paire gagne #2 ', () => {
+      expect(
+        pokerResult(
+          ['2H', '2S', '9S', 'AC', 'KD'],// double paire de 9 et de 2
+          ['2C', '3H', '3S', 'TC', 'TH']
+        )
+      ).to.be.equal('2');
+    });
+
+    it('la main avec la plus grande des 2 paires gagne ', () => {
+      expect(
+        pokerResult(
+          ['2H', '2S', '9S', '9C', 'KD'],
+          ['2C', '2H', 'TS', '5C', 'TH']
+        )
+      ).to.be.equal('2');
+    });
+
+    it('la main avec la plus grande des 2 paires gagne #2 ', () => {
+      expect(
+        pokerResult(
+          ['3H', '3S', '9S', '9C', 'KD'],
+          ['2C', '5H', '9S', '5C', '9H']
+        )
+      ).to.be.equal('2');
+    });
   });
-
-  it('la plus grande paire gagne', () => {
-    expect(pokerResult(['2H', '2S', '9S', '9C', 'KD'], ['2C', '3H', '4S', '4C', 'AH'])).to.be.equal('1');
-  }
-  )
-
-  // it('should evaluate a hand as high card', function () {
-  //   expect(false).to.be.true;
-  // });
-
-  // it('should evaluate a hand as a pair', function () {
-  //   expect(false).to.be.true;
-  // });
 });
