6abd76de74370231181643a2e19dfc061595ac5b
Nina-a <nina.leduc@gmail.com>
2021-01-12 18:29:52 +0100

    Flush WIP

--- poker.js
+++ poker.js
@@ -1,117 +1,205 @@
-let order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T','J','Q','K','A'];
+let order = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+
+let orderCombinaison = ['single', 'pair', 'doublePair', 'brelan', 'suite', 'couleur', 'full', 'carre', 'quinteFlush', 'quinteFlushRoyale'];
 
 function pokerResult(hand1, hand2) {
   // Remplir un tableau avec la valeur de la carte
+  let { numberOfSameCard1, numberOfSameCard2, ordreCard1, ordreCard2, suite1, suite2, couleur1, couleur2 } = initialisation(hand1, hand2);
+
+  // Il y a au moins une paire dans chaque main
+  if (
+    numberOfSameCard1.indexOf(2) !== -1 &&
+    numberOfSameCard2.indexOf(2) !== -1
+  ) {
+    return auMoins1PairDansChaqueMain(numberOfSameCard1, numberOfSameCard2, ordreCard1, ordreCard2 );
+    }
+
+  //Il y a au moins une paire dans une main et des cartes simples dans l'autre
+  else if (
+    !(numberOfSameCard1.indexOf(2) == -1 && numberOfSameCard2.indexOf(2) == -1)
+  ) {
+    if (suite1 || suite2)
+    {
+    let result = suite1 ? '1':'2';
+    return result;
+    }
+    else 
+    {
+    let result = numberOfSameCard1.indexOf(2) !== -1 ? '1' : '2';
+    return result;
+  }
+  }
+  // Il y a au moins un brelan dans chaque main
+  else if (numberOfSameCard1.indexOf(3) !== -1 &&
+  numberOfSameCard2.indexOf(3) !== -1) {
+    var brelan1 = numberOfSameCard1.indexOf(3);
+    var brelan2 = numberOfSameCard2.indexOf(3);
+
+    var result = brelan1 > brelan2 ? '1' : '2';
+
+    return result
+  }
+  // Il y a un brelan devant une double paire / paire / carte simple
+  else if(
+  !(numberOfSameCard1.indexOf(3) == -1 && numberOfSameCard2.indexOf(3) == -1)
+  ) {
+    if (suite1 || suite2)
+    {
+    let result = suite1 ? '1':'2';
+    return result;
+    }
+    else 
+    {
+    let result = numberOfSameCard1.indexOf(3) !== -1 ? '1' : '2';
+    return result;
+  }
+  }
+  // Il n'y a que des cartes simples dans les mains
+  else if (
+    numberOfSameCard1.indexOf(2) == -1 &&
+    numberOfSameCard1.indexOf(2) == -1
+  ) {
+    if (suite1 || suite2)
+    {
+    let result = suite1 ? '1':'2';
+    return result;
+    }
+    else 
+    {
+      return simpleCarte(ordreCard1, ordreCard2);
+  }
+  }
+}
+
+function initialisation(hand1, hand2) {
+  // Tableau donnant la valeur des cartes dans la main
   let valeurCardHand1 = [];
-  for (var i = 0; i < hand1.length; i++) 
-  {
+  for (var i = 0; i < hand1.length; i++) {
     let valeur = hand1[i].substr(0, 1);
     valeurCardHand1.push(valeur);
   }
 
+  // Tableau donnant les couleurs des cartes
+  let colorCardHand1 = new Map();
+  for (var i = 0; i < hand1.length; i++) {
+    let valeur = hand1[i].substr(1, 1);
+    colorCardHand1.set(valeur, i);
+  }
+  let couleur1 = colorCardHand1.size == 1 ;
+
+  // Tableau donnant la valeur des cartes dans la main
   let valeurCardHand2 = [];
-  for (var i = 0; i < hand2.length; i++) 
-  {
+  for (var i = 0; i < hand2.length; i++) {
     let valeur = hand2[i].substr(0, 1);
     valeurCardHand2.push(valeur);
   }
 
-  // tableau convertissant la valeur de la carte en ordre de la plus petite à la plus grande
-  let ordreCard1 =(valeurCardHand1.map((x) => order.indexOf(x)));
-  let ordreCard2 = (valeurCardHand2.map((x) => order.indexOf(x)));
+  // Tableau donnant les couleurs des cartes
+  let colorCardHand2 = new Map();
+  for (var i = 0; i < hand2.length; i++) {
+    let valeur = hand2[i].substr(1, 1);
+    colorCardHand2.set(valeur, i);
+  }
+  let couleur2 = colorCardHand2.size == 1 ;
+
+  // tableau convertissant et ordonnant la valeur de la carte en ordre de la plus petite à la plus grande
+  let ordreCard1 = valeurCardHand1.map((x) => order.indexOf(x));
+  let ordreCard2 = valeurCardHand2.map((x) => order.indexOf(x));
+  ordreCard1=ordreCard1.sort();
+  ordreCard2=ordreCard2.sort();
 
   // Tableau permettant de connaitre le nombre de fois ou l'on la même carte
   let numberOfSameCard1 = [];
-  for (var i = 0; i < ordreCard1.length; i++) 
-  {
-    if (numberOfSameCard1[ordreCard1[i]]) 
-    {
-      numberOfSameCard1[ordreCard1[i]] =
-        numberOfSameCard1[ordreCard1[i]] + 1;
-    }
-    else 
-    {
+  for (var i = 0; i < ordreCard1.length; i++) {
+    if (numberOfSameCard1[ordreCard1[i]]) {
+      numberOfSameCard1[ordreCard1[i]] = numberOfSameCard1[ordreCard1[i]] + 1;
+    } else {
       numberOfSameCard1[ordreCard1[i]] = 1;
     }
   }
 
   let numberOfSameCard2 = [];
-  for (var i = 0; i < ordreCard2.length; i++) 
-  {
-    if (numberOfSameCard2[ordreCard2[i]]) 
-    {
-      numberOfSameCard2[ordreCard2[i]] =
-        numberOfSameCard2[ordreCard2[i]] + 1;
-    } 
-    else 
-    {
+  for (var i = 0; i < ordreCard2.length; i++) {
+    if (numberOfSameCard2[ordreCard2[i]]) {
+      numberOfSameCard2[ordreCard2[i]] = numberOfSameCard2[ordreCard2[i]] + 1;
+    } else {
       numberOfSameCard2[ordreCard2[i]] = 1;
     }
   }
-
-  // Il y a au moins une paire dans chaque main
-  if ((numberOfSameCard1.indexOf(2) !== -1) && (numberOfSameCard2.indexOf(2) !== -1)) 
-  {
-    var paire1 = [];
-    var paire2 = [];
-
-    // trouver la paire par main
-    for (var valeur in numberOfSameCard1) 
-    {
-      if (numberOfSameCard1[valeur] == 2 ) 
-      {
-        paire1.push(valeur);
-      }
-    }
-    let maxPaire1 = Math.max(...paire1);
-    let nombreOfPaire1 = paire1.length;
   
-    for (var valeur in numberOfSameCard2) 
-    {
-      if (numberOfSameCard2[valeur] == 2) 
-      {
-        paire2.push(valeur);
-      }
-    }
-    let maxPaire2 = Math.max(...paire2);
-    let nombreOfPaire2 = paire2.length;
+  // Permet de savoir si on a une suite ou non
+  var suite1 = isSuite(numberOfSameCard1, ordreCard1);
+  var suite2 = isSuite(numberOfSameCard2, ordreCard2);
+  
+  return { numberOfSameCard1, numberOfSameCard2, ordreCard1, ordreCard2, suite1, suite2, couleur1, couleur2 };
+}
 
-    // Si les paires max ne sont pas les mêmes
-    if (( nombreOfPaire1 == nombreOfPaire2)  )
+function isSuite(numberOfSameCard, ordreCard) {
+  if (!((numberOfSameCard.indexOf(2) !== -1) && (numberOfSameCard.indexOf(3) !== -1))) {
+    let valeurMin1 = ordreCard[0];
+    var suite1 = false;
+    if (valeurMin1 !== 0) {
+      let valeurMax1 = ordreCard[4];
+      suite1 = valeurMax1 == valeurMin1 + 4;
+    }
+  }
+  return suite1;
+}
+
+function auMoins1PairDansChaqueMain(numberOfSameCard1, numberOfSameCard2, ordreCard1, ordreCard2 ) {
+  var paire1 = [];
+  var paire2 = [];
+
+  // trouver la paire par main
+  for (var valeur in numberOfSameCard1) {
+    if (numberOfSameCard1[valeur] == 2) {
+      paire1.push(valeur);
+    }
+  }
+  let maxPaire1 = Math.max(...paire1);
+  let nombreOfPaire1 = paire1.length;
+
+  for (var valeur in numberOfSameCard2) {
+    if (numberOfSameCard2[valeur] == 2) {
+      paire2.push(valeur);
+    }
+  }
+  let maxPaire2 = Math.max(...paire2);
+  let nombreOfPaire2 = paire2.length;
+
+  // Si le nombre de paire est le même
+  if (nombreOfPaire1 == nombreOfPaire2) {
+    // On regarde la paire la principale, Si c'est la même on regarde la seconde paire si y a une seconde paire
+    // On regarde la plus grande carte sinon
+    if (maxPaire1 == maxPaire2 && nombreOfPaire2 == 2) 
     {
-      if(maxPaire1 == maxPaire2 )
-      {
-        var result = paire1[0]>paire2[0] ? '1' : '2';
-      }
-      else
-      {
-        var result = maxPaire1 > maxPaire2 ? '1' : '2';
-      }
+      var result = paire1[0] > paire2[0] ? '1' : '2';
+    } 
+    else if (maxPaire1 == maxPaire2 && nombreOfPaire2 !== 2) 
+    {
+      let maxOrdreCard1 = Math.max(...ordreCard1);
+      let maxOrdreCard2 = Math.max(...ordreCard2);
+      let maxCard1 = maxOrdreCard1 == maxPaire1 ? ordreCard1[2] :maxOrdreCard1 ;
+      let maxCard2 = maxOrdreCard2 == maxPaire2 ? ordreCard2[2] :maxOrdreCard2
+      var result = maxCard1 > maxCard2 ? '1' : '2';
     }
     else
     {
       var result = maxPaire1 > maxPaire2 ? '1' : '2';
     }
-    return result;
+  } else {
+    var result = maxPaire1 > maxPaire2 ? '1' : '2';
   }
- //Il y a au moins une paire dans une main et des cartes simples dans l'autre
- else if(!((numberOfSameCard1.indexOf(2) == -1) && (numberOfSameCard2.indexOf(2) == -1)))
-  {
-
-    let result = numberOfSameCard1.indexOf(2) !== -1 ? '1': '2';
-    return result;
-  }
-  // Il n'y a que des cartes simples dans les mains
-  else if ((numberOfSameCard1.indexOf(2) == -1) && (numberOfSameCard1.indexOf(2) == -1)) 
-  {
-    // faire un tableau (fonction map) pour déterminer l'ordre des cartes
-
-    // Trouve la carte la plus grande
-    let maxcard1 = Math.max(...ordreCard1);
-    let maxcard2 = Math.max(...ordreCard2);
-
-    let result = maxcard1 > maxcard2 ? '1' : '2';
-    return result;
-  }
+  return result;
 }
+
+function simpleCarte(ordreCard1, ordreCard2) {
+  // faire un tableau (fonction map) pour déterminer l'ordre des cartes
+  let maxcard1 = Math.max(...ordreCard1);
+  let maxcard2 = Math.max(...ordreCard2);
+
+  let result = maxcard1 > maxcard2 ? '1' : '2';
+  return result;
+}
+
 export { pokerResult };
--- test/rules.tests.js
+++ test/rules.tests.js
@@ -3,98 +3,116 @@ import { pokerResult } from '../poker.js';
 const expect = require('chai').expect;
 
 describe('Poker Hand', function () {
+  describe('Single Cars', function () {
+    it('la plus haute carte gagne', () => {
 
-  describe('Single Cars', function(){
-    it('la plus haute carte gagne #1', () => {
-      expect(
-        pokerResult(
-          ['2H', '3D', '5S', '9C', 'KD'], // 1 gagne avec K
-          ['2C', '3H', '4S', '8C', 'QH']
-        )
-      ).to.be.equal('1');
-    });
+      let main1 = ['2H', '3H', '5H', '9H', 'KH'];
+      let main2 = ['2C', '3H', '4S', '8C', 'QH'];
 
-    it('la plus haute carte gagne #2', () => {
-      expect(
-        pokerResult(
-          ['2H', '3D', '5S', '9C', 'KD'],
-          ['2C', '3H', '4S', '8C', 'AH'] // 2 gagne avec A
-        )
-      ).to.be.equal('2');
+      testEtTestInverse(main1, main2);
     });
-  })
+  });
 
   describe('Pair', function () {
-    it('la plus grande paire gagne #1', () => {
-      expect(
-        pokerResult(
-          ['2H', '4S', '9S', 'KC', 'KD'], // 1 gagne avec la paire de roi
-          ['2C', '3H', '5S', '5C', 'AH']
-        )
-      ).to.be.equal('1');
+    it('la plus grande paire gagne', () => {
+      let main1 = ['2H', '2S', '9S', 'KC', 'KD'];
+      let main2 = ['2C', '3H', '4S', '4C', 'AH'];
+      // gagnant avec une paire de Roi contreune paire de 4
+      testEtTestInverse(main1, main2);
     });
 
-    it('la plus grande paire gagne #2', () => {
-      expect(
-        pokerResult(
-          ['2C', '3H', '4S', '4C', 'AH'],
-          ['2H', '2S', '9S', 'KC', 'KD'] // 2 gagne avec la paire de Roi
-        )
-      ).to.be.equal('2');
+    it('la main contenant au moins une paire contre des simples cartes gagne', () => {
+      let main1 = ['2H', '3S', '9S', '9C', 'KD'];
+      let main2 = ['2C', '3H', '4S', '5C', 'TH'];
+      // gagnant avec une paire de 9 contre des cartes simples
+      testEtTestInverse(main1, main2);
     });
 
-     it('la main contenant au moins une paire contre des simples cartes gagne #1', () => {
-      expect(
-        pokerResult(
-          ['2H', '3S', '9S', '9C', 'KD'], // gagne avec une paire de 9
-          ['2C', '3H', '4S', '5C', 'TH']
-        )
-      ).to.be.equal('1');
-    });
-
-    it('la main contenant au moins une paire contre des simples cartes gagne #2', () => {
-      expect(
-        pokerResult(
-          ['2H', '3S', '5S', '9C', 'KD'],
-          ['2C', '3H', '4S', 'TC', 'TH']// gagne avec une paire de 10
-        )
-      ).to.be.equal('2');
-    });
-
-    it('la main contenant 2 paires contre 1 paire gagne #1 ', () => {
-      expect(
-        pokerResult(
-          ['2H', '2S', '9S', '9C', 'KD'],// double paire de 9 et de 2
-          ['2C', '3H', '3S', '5C', 'TH']
-        )
-      ).to.be.equal('1');
-    });
-
-    it('la main contenant 2 paires contre 1 paire gagne #2 ', () => {
-      expect(
-        pokerResult(
-          ['2H', '2S', '9S', 'AC', 'KD'],// double paire de 9 et de 2
-          ['2C', '3H', '3S', 'TC', 'TH']
-        )
-      ).to.be.equal('2');
+    it('la main contenant 2 paires contre 1 paire gagne', () => {
+      let main1 =  ['2H', '2S', '9S', '9C', 'KD'];
+      let main2 =  ['2C', '3H', '3S', '5C', 'TH'];
+      // gagnant avec une paire de 9 et 2 contre une paire de 3
+      testEtTestInverse(main1, main2);
     });
 
     it('la main avec la plus grande des 2 paires gagne ', () => {
-      expect(
-        pokerResult(
-          ['2H', '2S', '9S', '9C', 'KD'],
-          ['2C', '2H', 'TS', '5C', 'TH']
-        )
-      ).to.be.equal('2');
+      let main1 = ['2C', '2H', 'TS', '5C', 'TH'];
+      let main2 = ['3H', '3S', '9S', '9C', 'KD'];
+      //gagnant paire de 10 et de 2 vs paire de 9 et de 3
+      testEtTestInverse(main1, main2);
     });
 
-    it('la main avec la plus grande des 2 paires gagne #2 ', () => {
-      expect(
-        pokerResult(
-          ['3H', '3S', '9S', '9C', 'KD'],
-          ['2C', '5H', '9S', '5C', '9H']
-        )
-      ).to.be.equal('2');
+    it('la main avec des paires identiques et la plus grande carte gagne ', () => {
+      let main1 = ['2C', '5H', 'TS', 'KC', 'TH'];
+      let main2 = ['3H', '7S', '9S', 'TC', 'TD'];
+      //gagnant paire de 10 plus grande carte Roi vs paire de 10 et plus grande carte 9
+      testEtTestInverse(main1, main2);
     });
   });
+ 
+  describe('Three of a Kind', function () {
+    it('le brelan le plus grand gagne', () => {
+      let main1 = ['2H', '3D', '9S', '9C', '9D'];
+      let main2 = ['2C', '3H', '8S', '8C', '8H'];
+      //gagnant avec un brelan de 9 par rapport au brelan de 8
+      testEtTestInverse(main1, main2);
+    });
+
+    it('le brelan gagne devant une double paire ou une paire', () => {
+      let main1 = ['2H', '3D', '9S', '9C', '9D'];
+      let main2 = ['2C', '3H', '8S', 'TC', 'AH'];
+      //gagnant avec un brelan de 9 par rapport au brelan de 8
+      testEtTestInverse(main1, main2);
+    });
+  });
+
+  describe('Straight', function () {
+    it('la suite gagne vs brelan', () => {
+      let main1 = ['3H', '4D', '5S', '6C', '7D'];
+      let main2 = ['2C', '3H', '8S', '8C', '8H'];
+      //gagnant avec une suite
+      testEtTestInverse(main1, main2);
+    });
+
+    it('la suite gagne vs paire/doublepaire', () => {
+      let main1 = ['3H', '4D', '5S', '6C', '7D'];
+      let main2 = ['2C', '3H', '3S', '8C', '8H'];
+      //gagnant avec une suite
+      testEtTestInverse(main1, main2);
+    });
+
+    it('la suite gagne vs carte simple ', () => {
+      let main1 = ['3H', '4D', '5S', '6C', '7D'];
+      let main2 = ['2C', '9H', '3S', 'TC', '8H'];
+      //gagnant avec une suite
+      testEtTestInverse(main1, main2);
+    });
+    });
+
+    describe('Couleur', function () {
+      it('la couleur gagne vs suite', () => {
+        let main1 = ['3H', '4D', '5S', '6C', '7D'];
+        let main2 = ['2C', '3C', '8C', 'TC', 'QC'];
+        //gagnant avec une suite
+        testEtTestInverse(main1, main2);
+      });
+      });
 });
+
+function testEtTestInverse(main1, main2) {
+  expect(
+    pokerResult(
+      main1,
+      main2
+      // 2 gagne avec la paire de Roi
+    )
+  ).to.be.equal('1');
+  expect(
+    pokerResult(
+      main2,
+      main1
+      // 2 gagne avec la paire de Roi
+    )
+  ).to.be.equal('2');
+}
+
